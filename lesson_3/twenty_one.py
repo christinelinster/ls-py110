afcddea49321bb391ceeba3e5aa1becaf4e6780e
Christine Lin <lin.christine19@gmail.com>
2025-05-07 13:02:21 -0400

    minimize calls to total function

--- lesson_3/twenty_one.py
+++ lesson_3/twenty_one.py
@@ -4,6 +4,8 @@ import os
 VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 SUITS = ['H', 'D', 'S', 'C']
 DECK = [[suit, value] for suit in SUITS for value in VALUES]
+BUST_VALUE = 21
+DEALER_HIT_VALUE = 17
 
 def prompt(message):
     print(f'==> {message}')
@@ -16,7 +18,7 @@ def initialize_deck():
 
 
 def total(cards):
-    values = [card[0] for card in cards]
+    values = [card[:-1] for card in cards]
 
     total_sum = 0
     for value in values:
@@ -34,13 +36,16 @@ def total(cards):
 
     return total_sum
 
-def busted(cards):
-    return total(cards) > 21
+
+def busted(total_value):
+    return total_value > BUST_VALUE
+
 
 def display_hand(cards):
     return ', '.join(cards)
 
-def player_turn(deck, player_cards):
+
+def player_turn(deck, player_cards, player_total):
     while True:
         answer = input('hit or stay? (h / s): ').lower()
         valid_choices = ('h', 's')
@@ -54,40 +59,40 @@ def player_turn(deck, player_cards):
             break
         if answer.startswith('h'):
             deal(deck, player_cards)
+            player_total = total(player_cards)
             prompt(f'Your cards are: {display_hand(player_cards)}')
-            prompt(f'Your total value is {total(player_cards)}')
-        if busted(player_cards):
-            break
+            prompt(f'Your total value is {player_total}')
 
-def dealer_turn(deck, dealer_cards):
+        if busted(player_total):
+            break
+    return player_total
+
+
+def dealer_turn(deck, dealer_cards, dealer_total):
     while True:
-        if total(dealer_cards) >= 17 or busted(dealer_cards):
+        if dealer_total >= DEALER_HIT_VALUE or busted(dealer_total):
             break
         deal(deck, dealer_cards)
-    if busted(dealer_cards):
-        prompt('The dealer bust. You win!')
+        dealer_total = total(dealer_cards)
+    return dealer_total
 
 
 def deal(deck, cards):
     cards.append(deck.pop())
 
+
 def deal_starting_cards(deck, player_cards, dealer_cards):
     for _ in range(2):
         player_cards.append(deck.pop())
         dealer_cards.append(deck.pop())
-    prompt(f'The dealer\'s cards are: ?, {dealer_cards[1]}')
-    prompt(f'Your cards are: {display_hand(player_cards)}')
-    prompt(f'Your total card value is: {total(player_cards)}')
 
-def calculate_results(player_cards, dealer_cards):
-    dealer_total = total(dealer_cards)
-    player_total = total(player_cards
-                         )
+
+def calculate_results(player_cards, dealer_cards, player_total, dealer_total):
     os.system('clear')
-    if busted(player_cards):
+    if player_total > BUST_VALUE:
         prompt('Busted! Dealer wins')
-    elif busted(dealer_cards):
-        prompt('You win! Dealer busted.')
+    elif dealer_total > BUST_VALUE:
+        prompt('Dealer busted. You win!')
     elif player_total > dealer_total:
         prompt('You win!')
     elif player_total < dealer_total:
@@ -95,13 +100,14 @@ def calculate_results(player_cards, dealer_cards):
     else:
         prompt('Tie game!')
 
-    display_results(player_cards, dealer_cards)
+    display_results(player_cards, dealer_cards, player_total, dealer_total)
 
-def display_results(player_cards, dealer_cards):
-    prompt(f'Your cards are: {display_hand(player_cards)}'
-           f'with a total value of {total(player_cards)}')
-    prompt(f'The dealer\'s cards are: {display_hand(dealer_cards)}'
-           f'with a total value of {total(dealer_cards)}')
+
+def display_results(player_cards, dealer_cards, player_total, dealer_total):
+    prompt(f'Your cards are: {display_hand(player_cards)} '
+           f'with a total value of {player_total}')
+    prompt(f'The dealer\'s cards are: {display_hand(dealer_cards)} '
+           f'with a total value of {dealer_total}')
 
 
 def play_twenty_one():
@@ -109,14 +115,21 @@ def play_twenty_one():
     current_deck = initialize_deck()
     player_cards = []
     dealer_cards = []
+
     deal_starting_cards(current_deck, player_cards, dealer_cards)
+    player_total = total(player_cards)
+    dealer_total = total(dealer_cards)
 
-    player_turn(current_deck, player_cards)
+    prompt(f'The dealer\'s cards are: ?, {dealer_cards[1]}')
+    prompt(f'Your cards are: {display_hand(player_cards)} with a total value of {player_total}')
+    
+    player_total = player_turn(current_deck, player_cards, player_total)
 
-    if not busted(player_cards):
-        dealer_turn(current_deck, dealer_cards)
+    if not busted(player_total):
+        dealer_total = dealer_turn(current_deck, dealer_cards, dealer_total)
 
-    calculate_results(player_cards, dealer_cards)
+    calculate_results(player_cards, dealer_cards, player_total, dealer_total)
+
 
 def play_again():
     prompt("Play again? (y or n)")
@@ -128,11 +141,11 @@ def play_again():
 
     return answer in ("y", "yes")
 
+
 def main():
     while True:
         play_twenty_one()
         if not play_again():
             break
-
 
 main()
