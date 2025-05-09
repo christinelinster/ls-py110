188daeb369f3a4c02178e65e4294cc6cc3a96423
Christine Lin <lin.christine19@gmail.com>
2025-05-09 13:30:26 -0400

    complete PY110 easy_3.9 exercise

---
+++ lesson_1/easy_3/easy_3.9.py
@@ -0,0 +1,36 @@
+# Reversed Lists
+
+
+def reverse_list(numbers):
+    l = 0
+    r = len(numbers) - 1
+
+    while l < r: 
+        numbers[l], numbers[r] = numbers[r], numbers[l]
+        l += 1
+        r -= 1
+
+    return numbers
+
+
+
+
+list1 = [1, 2, 3, 4]
+result = reverse_list(list1)
+print(result == [4, 3, 2, 1])               # True
+print(list1 is result)                      # True
+
+list2 = ["a", "b", "c", "d", "e"]
+result2 = reverse_list(list2)
+print(result2 == ['e', 'd', 'c', 'b', 'a']) # True
+print(list2 is result2)                     # True
+
+list3 = ["abc"]
+result3 = reverse_list(list3)
+print(result3 == ['abc'])                   # True
+print(list3 is result3)                     # True
+
+list4 = []
+result4 = reverse_list(list4)
+print(result4 == [])                        # True
+print(list4 is result4)                     # True
