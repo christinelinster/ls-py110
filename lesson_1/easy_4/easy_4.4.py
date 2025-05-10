e74d5bd7c2f64470c4927068b7e56c08c65d388c
Christine Lin <lin.christine19@gmail.com>
2025-05-10 14:24:03 -0400

    complete PY110 easy_4.4 exercise

---
+++ lesson_1/easy_4/easy_4.4.py
@@ -0,0 +1,13 @@
+# Arrange a Dictionary
+
+def order_by_value(my_dict):
+    sorted_items = sorted(my_dict.items(), key=sort_by_value)
+    return [key for key, value in sorted_items]
+        
+    
+def sort_by_value(item):
+    return item[1]
+
+my_dict = {'p': 8, 'q': 2, 'r': 6}
+keys = ['q', 'r', 'p']
+print(order_by_value(my_dict) == keys)  # True
